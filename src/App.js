88b80d86f5ac08ab4b53362c18c41018190012ec
Boris Bosiljcic <b.bosiljcic@gmail.com>
2020-10-05 18:32:23 +0200

    cra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import pizzaService from './services/pizza';
+
+function App() {
+  return (
+    <div className="App">
+      TEST
+    </div>
+  );
+}
+
+export default App;
